447ca31ce36e256a6843fe3498c6a2d887e33cb6
Alexander Verbruggen <alex@nabu.be>
2023-02-05 10:50:13 +0100

    updated

--- src/main/java/be/nabu/eai/module/auditing/DynamicRuntimeTracker.java
+++ src/main/java/be/nabu/eai/module/auditing/DynamicRuntimeTracker.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import be.nabu.eai.module.auditing.api.FlatServiceTracker;
 import be.nabu.eai.module.auditing.api.FlatServiceTracker.TrackTimeType;
-import be.nabu.eai.module.auditing.api.FlatServiceTracker.TrackType;
+import be.nabu.libs.services.MultipleServiceRuntimeTracker;
 import be.nabu.libs.services.ServiceRuntime;
 import be.nabu.libs.services.ServiceUtils;
 import be.nabu.libs.services.api.DefinedService;
@@ -19,8 +19,9 @@ import be.nabu.libs.services.pojo.POJOUtils;
 
 public class DynamicRuntimeTracker implements ServiceRuntimeTrackerProvider {
 
-	private Map<String, DefinedService> trackers = new ConcurrentHashMap<String, DefinedService>();
+	private Map<String, List<DefinedService>> trackers = new ConcurrentHashMap<String, List<DefinedService>>();
 	private MethodServiceInterface trackInterface;
+	private boolean recursive;
 	
 	public DynamicRuntimeTracker() {
 		trackInterface = MethodServiceInterface.wrap(FlatServiceTracker.class, "track");
@@ -28,53 +29,128 @@ public class DynamicRuntimeTracker implements ServiceRuntimeTrackerProvider {
 	
 	@Override
 	public ServiceRuntimeTracker getTracker(ServiceRuntime runtime) {
-		boolean track = false;
 		ServiceRuntime runtimeToCheck = runtime;
-		DefinedService serviceTracker = null;
+		MultipleServiceRuntimeTracker serviceTracker = null;
+		
+		String key = "dynamic-audit-" + (recursive ? "recursive" : "non-recursive");
+		// if we already have a tracker, we don't want to register a new one because this would mess up run ids etc
+		// this is meant for long term auditing, so if you add an auditer _while_ the target is already running, too bad, you are missing that run
+		MultipleServiceRuntimeTracker currentTracker = (MultipleServiceRuntimeTracker) runtime.getContext().get(key);
+		boolean shouldTrack = false;
+		
 		while (runtimeToCheck != null) {
 			// if we meet a tracking service, the service is a child from the tracker itself, do not track
 			if (POJOUtils.isImplementation(runtimeToCheck.getService(), trackInterface)) {
-				track = false;
 				break;
 			}
 			Service unwrap = ServiceUtils.unwrap(runtimeToCheck.getService());
 			// check unwrapped as well (see fixed value wrapping)
 			if (POJOUtils.isImplementation(runtimeToCheck.getService(), trackInterface)) {
-				track = false;
 				break;
 			}
 			if (unwrap instanceof DefinedService && trackers.containsKey(((DefinedService) unwrap).getId())) {
-				track = true;
-				serviceTracker = trackers.get(((DefinedService) unwrap).getId());
+				List<DefinedService> list = trackers.get(((DefinedService) unwrap).getId());
+				if (list != null && !list.isEmpty()) {
+					shouldTrack = true;
+					// only go through the trouble of constructing an actual tracker if we don't have one yet
+					if (currentTracker == null) {
+						List<ServiceRuntimeTracker> tracker = new ArrayList<ServiceRuntimeTracker>();
+						for (DefinedService single : list) {
+							FlatServiceTrackerWrapper wrapper = new FlatServiceTrackerWrapper(single, runtime.getExecutionContext());
+							wrapper.setIncludeServices(true);
+							wrapper.setIncludeSteps(false);
+							wrapper.setIncludeReports(false);
+							wrapper.setTimeType(TrackTimeType.ALL);
+							wrapper.setIncludeDescriptions(true);
+							tracker.add(wrapper);
+						}
+						serviceTracker = new MultipleServiceRuntimeTracker(tracker.toArray(new ServiceRuntimeTracker[tracker.size()]));
+					}
+				}
+				break;
+			}
+			else if (!recursive) {
 				break;
 			}
 			else {
 				runtimeToCheck = runtimeToCheck.getParent();
 			}
 		}
-		if (track) {
-			// check if we already have a tracker, share it across all? this makes it harder to have multiple different types of trackers though...
-			FlatServiceTrackerWrapper tracker = (FlatServiceTrackerWrapper) runtime.getContext().get("dynamic-audit");
-			if (tracker == null) {
-				tracker = new FlatServiceTrackerWrapper(serviceTracker, runtime.getExecutionContext());
-				tracker.setType(TrackType.SERVICE);
-				tracker.setTimeType(TrackTimeType.ALL);
-				runtime.getContext().put("dynamic-audit", tracker);
+		if (shouldTrack) {
+			if (currentTracker == null) {
+				runtime.getContext().put(key, serviceTracker);
+				return serviceTracker;
 			}
-			return tracker;
+			else {
+				return currentTracker;
+			}
 		}
 		return null;
 	}
 
 	public void auditService(String serviceId, DefinedService trackerService) {
-		trackers.put(serviceId, trackerService);
+		if (!trackers.containsKey(serviceId)) {
+			trackers.put(serviceId, new ArrayList<DefinedService>());
+		}
+		boolean found = false;
+		// we only want one instance for each combo
+		for (DefinedService single : trackers.get(serviceId)) {
+			if (single.getId().equals(trackerService.getId())) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			trackers.get(serviceId).add(trackerService);
+		}
 	}
 	
 	public void unauditService(String serviceId) {
 		trackers.remove(serviceId);
 	}
 	
-	public List<String> getAudited() {
-		return new ArrayList<String>(trackers.keySet());
+	public void unauditService(String serviceId, DefinedService trackerService) {
+		List<DefinedService> list = trackers.get(serviceId);
+		if (list != null) {
+			for (DefinedService single : trackers.get(serviceId)) {
+				if (single.getId().equals(trackerService.getId())) {
+					list.remove(single);
+					break;
+				}
+			}
+		}
+		if (list != null && list.isEmpty()) {
+			trackers.remove(serviceId);
+		}
 	}
+	
+	public List<String> getAudited(String auditServiceId) {
+		if (auditServiceId == null) {
+			return new ArrayList<String>(trackers.keySet());
+		}
+		else {
+			ArrayList<String> result = new ArrayList<String>();
+			for (Map.Entry<String, List<DefinedService>> entry : trackers.entrySet()) {
+				List<DefinedService> value = entry.getValue();
+				if (value != null) {
+					for (DefinedService service : value) {
+						if (service.getId().equals(auditServiceId)) {
+							result.add(entry.getKey());
+							break;
+						}
+					}
+				}
+			}
+			return result;
+		}
+	}
+
+	public boolean isRecursive() {
+		return recursive;
+	}
+
+	public void setRecursive(boolean recursive) {
+		this.recursive = recursive;
+	}
+	
 }
--- src/main/java/nabu/misc/auditing/Services.java
+++ src/main/java/nabu/misc/auditing/Services.java
@@ -5,10 +5,10 @@ import java.util.List;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.validation.constraints.NotNull;
 
 import be.nabu.eai.module.auditing.DynamicRuntimeTracker;
 import be.nabu.eai.module.auditing.FlatServiceTrackerWrapper;
-import be.nabu.eai.module.auditing.api.FlatServiceTracker.TrackType;
 import be.nabu.eai.repository.EAIResourceRepository;
 import be.nabu.eai.repository.api.ModifiableServiceRuntimeTrackerProvider;
 import be.nabu.libs.artifacts.api.Artifact;
@@ -23,15 +23,19 @@ public class Services {
 	
 	private ExecutionContext executionContext;
 	private static DynamicRuntimeTracker instance;
+	private static DynamicRuntimeTracker nonRecursiveInstance;
 	
 	/**
 	 * Allows you to register a service that performs service tracking, it must implement the interface nabu.interfaces.Services.track
 	 */
-	public void registerServiceTracker(@WebParam(name = "serviceId") String serviceId, @WebParam(name = "trackType") TrackType trackType, @WebParam(name = "recursive") Boolean recursive) {
+	public void registerServiceTracker(@WebParam(name = "serviceId") String serviceId, @WebParam(name = "recursive") Boolean recursive, @WebParam(name = "includeServices") Boolean includeServices, @WebParam(name = "includeSteps") Boolean includeSteps, @WebParam(name = "includeReports") Boolean includeReports, @WebParam(name = "includeDescriptions") Boolean includeDescriptions) {
 		DefinedService resolved = executionContext.getServiceContext().getResolver(DefinedService.class).resolve(serviceId);
 		if (executionContext.getServiceContext().getServiceTrackerProvider() instanceof ModifiableServiceRuntimeTrackerProvider) {
 			FlatServiceTrackerWrapper runtimeTracker = new FlatServiceTrackerWrapper(resolved, executionContext);
-			runtimeTracker.setType(trackType == null ? TrackType.SERVICE : trackType);
+			runtimeTracker.setIncludeDescriptions(includeDescriptions);
+			runtimeTracker.setIncludeReports(includeReports);
+			runtimeTracker.setIncludeServices(includeServices);
+			runtimeTracker.setIncludeSteps(includeSteps);
 			((ModifiableServiceRuntimeTrackerProvider) executionContext.getServiceContext().getServiceTrackerProvider()).addTracker(
 				ServiceRuntime.getRuntime().getService(), 
 				runtimeTracker, 
@@ -43,7 +47,7 @@ public class Services {
 		}
 	}
 	
-	public void auditService(@WebParam(name = "serviceId") String serviceId, @WebParam(name = "auditServiceId") String auditServiceId) {
+	public void auditService(@NotNull @WebParam(name = "serviceId") String serviceId, @NotNull @WebParam(name = "auditServiceId") String auditServiceId, @WebParam(name = "recursive") Boolean recursive) {
 		Artifact serviceToTrack = EAIResourceRepository.getInstance().resolve(serviceId);
 		Artifact auditService = EAIResourceRepository.getInstance().resolve(auditServiceId);
 		if (!(serviceToTrack instanceof DefinedService)
@@ -51,28 +55,69 @@ public class Services {
 				|| !POJOUtils.isImplementation((DefinedService) auditService, MethodServiceInterface.wrap(be.nabu.eai.module.auditing.api.FlatServiceTracker.class, "track"))) {
 			throw new IllegalArgumentException("Invalid input, can not track " + serviceId + " with " + auditServiceId);
 		}
-		if (instance == null) {
-			synchronized(Services.class) {
-				if (instance == null) {
-					DynamicRuntimeTracker dynamicRuntimeTracker = new DynamicRuntimeTracker();
-					// set up a new service track provider
-					((EAIResourceRepository) EAIResourceRepository.getInstance()).getDynamicRuntimeTrackers().add(dynamicRuntimeTracker);
-					instance = dynamicRuntimeTracker;
+		if (recursive == null || recursive) {
+			if (instance == null) {
+				synchronized(Services.class) {
+					if (instance == null) {
+						DynamicRuntimeTracker dynamicRuntimeTracker = new DynamicRuntimeTracker();
+						dynamicRuntimeTracker.setRecursive(true);
+						// set up a new service track provider
+						((EAIResourceRepository) EAIResourceRepository.getInstance()).getDynamicRuntimeTrackers().add(dynamicRuntimeTracker);
+						instance = dynamicRuntimeTracker;
+					}
 				}
 			}
+			instance.auditService(serviceId, (DefinedService) auditService);
 		}
-		instance.auditService(serviceId, (DefinedService) auditService);
+		else {
+			if (nonRecursiveInstance == null) {
+				synchronized(Services.class) {
+					if (nonRecursiveInstance == null) {
+						DynamicRuntimeTracker dynamicRuntimeTracker = new DynamicRuntimeTracker();
+						dynamicRuntimeTracker.setRecursive(false);
+						// set up a new service track provider
+						((EAIResourceRepository) EAIResourceRepository.getInstance()).getDynamicRuntimeTrackers().add(dynamicRuntimeTracker);
+						nonRecursiveInstance = dynamicRuntimeTracker;
+					}
+				}
+			}
+			nonRecursiveInstance.auditService(serviceId, (DefinedService) auditService);
+		}
 	}
 	
-	public void unauditService(@WebParam(name = "serviceId") String serviceId) {
-		if (instance != null) {
-			instance.unauditService(serviceId);
+	public void unauditService(@NotNull @WebParam(name = "serviceId") String serviceId, @WebParam(name = "auditServiceId") String auditServiceId, @WebParam(name = "recursive") Boolean recursive) {
+		if ((recursive == null || recursive) && instance != null) {
+			if (auditServiceId == null) {
+				instance.unauditService(serviceId);
+			}
+			else {
+				instance.unauditService(serviceId, (DefinedService) EAIResourceRepository.getInstance().resolve(auditServiceId));
+			}
+		}
+		else if (recursive != null && !recursive && nonRecursiveInstance != null) {
+			if (auditServiceId == null) {
+				nonRecursiveInstance.unauditService(serviceId);
+			}
+			else {
+				nonRecursiveInstance.unauditService(serviceId, (DefinedService) EAIResourceRepository.getInstance().resolve(auditServiceId));
+			}
 		}
 	}
 	
 	@WebResult(name = "auditedServices")
 	// list the services that are currently being audited
-	public List<String> listAudited() {
-		return instance == null ? null : instance.getAudited();
+	public List<String> listAudited(@WebParam(name = "auditServiceId") String auditServiceId, @WebParam(name = "recursive") Boolean recursive) {
+		if (recursive == null || recursive) {
+			if (instance == null) {
+				return null;
+			}
+			return instance.getAudited(auditServiceId);
+		}
+		else {
+			if (nonRecursiveInstance == null) {
+				return null;
+			}
+			return nonRecursiveInstance.getAudited(auditServiceId);
+		}
 	}
 }
